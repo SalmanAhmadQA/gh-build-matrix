4620232f75f02aa835b4c984d434d4785a21cbf3
SalmanAhmadQA <142823513+SalmanAhmadQA@users.noreply.github.com>
2023-12-12 16:15:29 +0500

    Update entrypoint.sh

--- entrypoint.sh
+++ entrypoint.sh
@@ -4,7 +4,7 @@ N=$1
 # by default N is 1
 N=${N:-1}
 
-INDEX=1
+INDEX=0
 
 OUTPUT='{"containers":['
 
@@ -12,9 +12,9 @@ while [ $INDEX -lt $N ]
 do
   if [ $INDEX -lt $(($N-1)) ]
   then
-    OUTPUT+="$(($INDEX)), "
+    OUTPUT+="$(($INDEX+1)), "
   else
-    OUTPUT+="$(($INDEX))"
+    OUTPUT+="$(($INDEX+1))"
   fi
 
   INDEX=$(($INDEX+1))
